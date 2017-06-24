1e192178a123b272aaacac1d4b043b86ae0144bc
Unknown <G.Rugenbrink@kpnmail.nl>
2017-06-24 21:55:21 +0200

    CRUD
    Added all the CRUD elements of the API

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,51 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db){
-	app.post('/notes', (req, res) =>{
-		//create note here
-		console.log(req.body);
-		res.send('Hello')
+	app.get('/catfacts/:id', (req, res) => {
+    	const id = req.params.id;
+    	const details = { '_id': new ObjectID(id) };
+    	db.collection('catfacts').findOne(details, (err, item) => {
+	      if (err) {
+	        res.send({'error':'An error has occurred'});
+	      } else {
+	        res.send(item);
+	      } 
+    	});
+  	});
+
+  	app.delete('/catfacts/:id', (req, res) => {
+    	const id = req.params.id;
+    	const details = { '_id': new ObjectID(id) };
+    	db.collection('catfacts').remove(details, (err, item) => {
+	      if (err) {
+	        res.send({'error':'An error has occurred'});
+	      } else {
+	        res.send('Catfact' + id + ' deleted!');
+	      } 
+    	});
+  	});
+
+  	app.put('/catfacts/:id', (req, res) =>{
+  		const id = req.params.id;
+  		const details = {	'_id': new ObjectID(id)	};
+  		const note = {	text: req.body.body	};
+  		db.collection('catfacts').update(details, note, (err, result) => {
+  			if(err){
+  				res.send({'error': 'An error has occurred!'});
+  			} else {
+  				res.send(note);
+  			}
+  		});
+  	});
+
+	app.post('/catfacts', (req, res) =>{
+		const catfact = {	text: req.body.body	};
+		db.collection('catfacts').insert(catfact, (err, result) =>{
+			if(err){
+				res.send({	'error': 'An error has occurred'	});
+			} else {
+				res.send(result.ops[0]);
+			}
+		});
 	});
 };
